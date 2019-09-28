7b2d53c3b70deddf25fe1660904e0f3ab7ffb9bb
popoway <31576265+popoway@users.noreply.github.com>
2019-09-28 06:44:03 -0400

    add snap-scrolling for questionnaire

--- sar/src/app/second-screen/second-screen.component.ts
+++ sar/src/app/second-screen/second-screen.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondScreenComponent implements OnInit {
 
+  // console.log(document);
+  // (<HTMLInputElement>document).querySelector("input").addEventListener("click", function() {
+  //   alert("Hello, World!");
+  // });
+
+  onClick() {
+    alert("hello, world");
+  }
+
   constructor() { }
 
   ngOnInit() {
