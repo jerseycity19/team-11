011e2eb387c9f0a398c5a0f9a759747d19fa0eb5
kaylyntorres <kaylyn.torres16@gmail.com>
2019-09-28 00:37:58 -0400

    Add first-screen component

--- sar/src/app/app.module.ts
+++ sar/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstScreenComponent } from './first-screen/first-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstScreenComponent
   ],
   imports: [
     BrowserModule,
