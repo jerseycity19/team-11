e2d3fe329746b55b7af3ebde97f2f7c2d11f895f
Nisarg Desai <nisarg.desai599@gmail.com>
2019-09-28 03:22:51 -0400

    Temp push

--- sar/src/app/app-routing.module.ts
+++ sar/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SecondScreenComponent } from './second-screen/second-screen.component';
 import { ThirdScreenComponent } from './third-screen/third-screen.component';
+import { FirstScreenComponent } from './first-screen/first-screen.component';
 
 const routes: Routes = [
+  { path: 'first-screen', component: FirstScreenComponent },
   { path: 'second-screen', component: SecondScreenComponent },
   { path: 'third-screen', component: ThirdScreenComponent },
   // { path: 'hero/:id', component: HeroDetailComponent },
@@ -14,7 +16,7 @@ const routes: Routes = [
   // },
   {
     path: '',
-    redirectTo: '/second-screen',
+    redirectTo: '/first-screen',
     pathMatch: 'full'
   },
   // { path: '**', component: PageNotFoundComponent }
--- sar/src/app/app.module.ts
+++ sar/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstScreenComponent } from './first-screen/first-screen.component';
 import { SecondScreenComponent } from './second-screen/second-screen.component';
 import { ThirdScreenComponent } from './third-screen/third-screen.component';
+import { CarouselPageComponent } from './carousel-page/carousel-page.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { ThirdScreenComponent } from './third-screen/third-screen.component';
     FirstScreenComponent,
     SecondScreenComponent,
     ThirdScreenComponent,
+    CarouselPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
